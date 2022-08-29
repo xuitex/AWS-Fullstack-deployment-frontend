6e004b86c707be7f4963a525eb775af2c9ba5dff
Armaan <46106298+xuitex@users.noreply.github.com>
2022-08-30 00:29:40 +0530

    added tests and buildspec.yml file

--- src/tests/square.test.js
+++ src/tests/square.test.js
@@ -4,7 +4,7 @@ test("Should calculate square of number", () => {
   const squaredValue = square(4);
 
   //   if (squaredValue != 16) {
-  //     throw new Error(`Squared value was ${squaredValue}, should have been 16`);
+  ///     throw new Error(`Squared value was ${squaredValue}, should have been 16`);
   //   }
 
   expect(squaredValue).toBe(16);
